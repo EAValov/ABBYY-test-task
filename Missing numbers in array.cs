663f1c5d49dcf689cd76e5f6f67ac3d2e371ec81
Valov <eugene.valov@gmail.com>
2020-07-17 23:48:29 +0500

    Добавил реализацию

--- Missing numbers in array.cs
+++ Missing numbers in array.cs
@@ -1,28 +1,52 @@
-Developing buisness systems:
-1. Product configurator for sales department (Complete transformer substations and related equipment). As I know, this software is most advanced in Russia within the same.
-2. Price Management and Product Pricing System.
-3. Query optimization and refactoring for legacy ERP, CRM etc.
+using System;
 
-System integration:
-1. With colleagues we redeveloped old chaotic integrations with new system, based on ESP (RabbitMQ) and Web API.
-2. Made convenient tools for integrating all of our platforms. 
-3. Developers was trained of using enterprise integration patterns.
+class Program
+{
+	/// <summary>
+	/// В массиве длины n - 1 встречаются все числа от 0 до n, кроме двух.
+	/// За время порядка n найти эти числа.
+	/// Можно использовать только не зависящее от n число байт дополнительной памяти. 
+	/// </summary>
+	public static void Main()
+	{
+		int[] arr = { 1, 7, 4, 0, 5, 6 };
 
-Infrastructure and DevOps:
-1. Created and implemented the practice of developing microservices and Continuous Delivery based on TFS 2018.
-2. Developed an application for deploying test environments in one step.
-3. Developed tools for convenient logging and working with logs - viewing and analysis, reports, notifications.
-4. Created a local repository of .nuget packages, configured the build and trained the developers.
+		// дополнительный массив для отстутвующих элементов.
+		int[] helper = new int[2];
+		
+		// Ключ для проверки наличия 0 в исходном массиве.
+		bool is_zero_found = false;
 
-Together with system analysts, we developing and analyzing the technical architecture using Archimate.
-We use: ASP.NET MVC + AngularJS, WCF, WPF / WinForms, SQL Server 2017, Oracle 11g. ESB on RabiitMQ. New projects on .Net Core 2.1, Redis and React.
+		// проходим по массиву и отмечаем элементы (делая их отрицательными).
+		for (int i = 0; i < arr.Length; i++)
+		{
+			int val = Math.Abs(arr[i]);
 
-I like C# for the expressiveness and nice features.
-Quickly and independently study new things. Challenges gives me an incentive to develop myself.
-Most of all I have been working with .NET and related technologies - Sql Server, ASP.NET MVC, WCF, WPF/WinForms. 
-I prefer to write queries by hands + Dapper. ORM is nice, but my approach seems to me easier and it works faster.
-Try to stick SOLID and common sense, apply design patterns without fanaticism. Try to use Unit tests and TDD when it's possible.
-I have an experience of making DIY electronic devices with Arduino, thats why i'm always careful with memory consumption and expensive algorithms.
-Read the professional literature, collect experience and best practices.
+			 // если значение элемента в границе исходного массива,
+			if (val < arr.Length)
+			{
+				arr[val] *= (-1);
 
-Got everything i need to work remotely - a new PC, quiet working conditions, all types of communication and reliable connection with backup uplink.
+				// отмечаем наличие 0 в исходном массиве, т.к. его нельзя отметить, сделав отрицательным.
+				if (arr[val] == 0)
+					is_zero_found = true;
+			}
+			else
+			{
+				// добавляем элементы в хелпер, значение которых выходит за границы массива.
+				helper[val % arr.Length] = -1;
+			}
+		}
+
+		// Выводим те индексы, на которых значение не было отмечено, т.е. больше 0
+		for (int i = 0; i < arr.Length; i++)
+			if (is_zero_found ? arr[i] > 0 : arr[i] >= 0)
+				Console.Write(i + " ");
+			
+		// Выводим индекс вспомогательного массива с неотмеченными значениями + размер исходного массива.
+		for (int i = 0; i < helper.Length; i++)
+			if (helper[i] >= 0)
+				Console.Write(arr.Length + i + " ");
+	}
+}
+
