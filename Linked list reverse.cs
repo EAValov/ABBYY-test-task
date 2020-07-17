26b31daabdd4368748aa496af5736918d3cf50e3
Valov <eugene.valov@gmail.com>
2020-07-17 23:46:51 +0500

    Добавил реализацию

---
+++ Linked list reverse.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+class Program
+{
+	/// <summary>
+	/// Инвертировать односвязный список за линейное время и
+	/// константную дополнительную память.
+	/// </summary>
+	static void Main(string[] args)
+	{
+		LinkedList<char> list = new LinkedList<char>();
+
+		list.Add('A');
+		list.Add('B');
+		list.Add('C');
+
+		list.PrintList();
+		list.Reverse();
+		list.PrintList();
+	}
+
+	class LinkedList<T>
+	{
+		Node head;
+
+		class Node
+		{
+			public T data;
+			public Node next;
+
+			public Node(T _data)
+			{
+				data = _data;
+				next = null;
+			}
+		}
+
+		public void Add(T data)
+		{
+			if (head == null)
+				head = new Node(data);
+			else
+			{
+				Node temp = head;
+
+				while (temp.next != null)
+					temp = temp.next;
+
+				temp.next = new Node(data);
+			}
+		}
+
+		public void Reverse()
+		{
+			Node prev = null;
+			Node current = head;
+			Node next = null;
+
+			while (current != null)
+			{
+				next = current.next;
+				current.next = prev;
+				prev = current;
+				current = next;
+			}
+
+			head = prev;
+		}
+
+		public void PrintList()
+		{
+			Node current = head;
+			
+			while (current != null)
+			{
+				Console.Write(current.data.ToString() + " ");
+				current = current.next;
+			}
+			
+			Console.WriteLine();
+		}
+	}
+}
+
+
---
+++ Missing numbers in array.cs
@@ -0,0 +1,28 @@
+Developing buisness systems:
+1. Product configurator for sales department (Complete transformer substations and related equipment). As I know, this software is most advanced in Russia within the same.
+2. Price Management and Product Pricing System.
+3. Query optimization and refactoring for legacy ERP, CRM etc.
+
+System integration:
+1. With colleagues we redeveloped old chaotic integrations with new system, based on ESP (RabbitMQ) and Web API.
+2. Made convenient tools for integrating all of our platforms. 
+3. Developers was trained of using enterprise integration patterns.
+
+Infrastructure and DevOps:
+1. Created and implemented the practice of developing microservices and Continuous Delivery based on TFS 2018.
+2. Developed an application for deploying test environments in one step.
+3. Developed tools for convenient logging and working with logs - viewing and analysis, reports, notifications.
+4. Created a local repository of .nuget packages, configured the build and trained the developers.
+
+Together with system analysts, we developing and analyzing the technical architecture using Archimate.
+We use: ASP.NET MVC + AngularJS, WCF, WPF / WinForms, SQL Server 2017, Oracle 11g. ESB on RabiitMQ. New projects on .Net Core 2.1, Redis and React.
+
+I like C# for the expressiveness and nice features.
+Quickly and independently study new things. Challenges gives me an incentive to develop myself.
+Most of all I have been working with .NET and related technologies - Sql Server, ASP.NET MVC, WCF, WPF/WinForms. 
+I prefer to write queries by hands + Dapper. ORM is nice, but my approach seems to me easier and it works faster.
+Try to stick SOLID and common sense, apply design patterns without fanaticism. Try to use Unit tests and TDD when it's possible.
+I have an experience of making DIY electronic devices with Arduino, thats why i'm always careful with memory consumption and expensive algorithms.
+Read the professional literature, collect experience and best practices.
+
+Got everything i need to work remotely - a new PC, quiet working conditions, all types of communication and reliable connection with backup uplink.
